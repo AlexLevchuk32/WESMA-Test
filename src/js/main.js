e7ab89d48b5efadde8602f2cf608b78972037a7a
Alex Levchuk <alexlevchuk@mail.ru>
2024-04-29 12:50:16 +0300

    add swiper slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import '../scss/style.scss';
+import Swiper from 'swiper/bundle';
+
+window.addEventListener('DOMContentLoaded', () => {
+	//=========================================================================================================================================================
+	// Swiper
+	const ourCasesSlider = new Swiper('.our-cases-slider', {
+		loop: true,
+		// autoplay: {
+		// 	delay: 5000,
+		// },
+		keyboard: {
+			enable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+});
