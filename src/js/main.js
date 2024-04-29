c551957ed5867c323017f4f1570b169b0acc9196
Alex Levchuk <alexlevchuk@mail.ru>
2024-04-29 21:48:08 +0300

    add style for funnel slide

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,25 @@
 import '../scss/style.scss';
 import Swiper from 'swiper/bundle';
 
-window.addEventListener('DOMContentLoaded', () => {
-	//=========================================================================================================================================================
-	// Swiper
-	const ourCasesSlider = new Swiper('.our-cases-slider', {
-		loop: true,
-		// autoplay: {
-		// 	delay: 5000,
-		// },
-		keyboard: {
-			enable: true,
+//=========================================================================================================================================================
+// Swiper
+const ourCasesSlider = new Swiper('.our-cases-slider', {
+	loop: true,
+	// autoplay: {
+	// 	delay: 5000,
+	// },
+	keyboard: {
+		enable: true,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
 		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span class="' + className + '">' + (index + 1) + '</span>';
-			},
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
