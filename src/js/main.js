c286620737904999b3388518bb24d68ec9d029ed
Alex Levchuk <alexlevchuk@mail.ru>
2024-04-29 14:19:13 +0300

    add funnel slide

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
 		keyboard: {
 			enable: true,
 		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<span class="' + className + '">' + (index + 1) + '</span>';
+			},
+		},
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
